986eeddce37737950ef6e5dbc3dba386f764b25a
Josh Vinlove <jjvinlove@gmail.com>
2019-12-04 08:14:11 -0700

    background image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Image } from 'semantic-ui-react'
+// import Register from './Register';
+// import Login from './Login';
+// import TourneyContainer from './TourneyContainer';
+// import HeaderComponent from './HeaderComponent';
+// import CreateIssue from './CreateIssueForm'; //I do not think we can do this.
+import { Route, Switch } from 'react-router-dom';
+import Pickleball from'./PickleballOnLine.png';
+
+
+const My404 = () => {
+	return (
+		<div>
+			<h3>You are lost.</h3>
+		</div>
+	)
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Placeholder</p>
-      </header>
-    </div>
-  );
+  
+  		<div>
+  			<h1>Lose</h1><h1>Again</h1>
+
+		</div>
+	
+    )
 }
 
 export default App;
